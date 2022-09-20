9dc1c8c1445fe8db1b8e4712abab366bee4832bf
Ashley <ashuhleyt@gmail.com>
2022-09-20 08:25:13 -0700

    feat: return vendor name

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -34,4 +34,11 @@ RSpec.describe Market do
 
     expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
   end
+
+  it 'can return the names of vendors' do 
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+  end
 end
