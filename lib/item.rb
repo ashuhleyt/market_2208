80dcf82fde528617c4d8615b85036c3f46598753
Ashley <ashuhleyt@gmail.com>
2022-09-20 08:06:11 -0700

    feat: attributes

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,9 @@
 class Item 
-
+  attr_reader :name,
+              :price
 
   def initialize(attributes)
-    
+    @name = attributes[:name]
+    @price = attributes[:price].delete_prefix('$').to_f
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -10,5 +10,8 @@ RSpec.describe Item do
   it 'exists and has readable attributes' do 
     expect(@item1).to be_instance_of(Item)
     expect(@item2).to be_instance_of(Item)
+
+    expect(@item2.name).to eq('Tomato')
+    expect(@item2.price).to eq(0.5)
   end
 end
