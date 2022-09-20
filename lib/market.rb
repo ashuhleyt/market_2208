479da76795ed505e9a99ee9ad1525445b91cc7f5
Ashley <ashuhleyt@gmail.com>
2022-09-20 10:29:22 -0700

    feat: vendors that sell

--- lib/market.rb
+++ lib/market.rb
@@ -12,10 +12,34 @@ class Market
   end
 
   def vendor_names
-    names = @vendors.map { |vendor| vendor.name}
+    names = @vendors.map { |vendor| vendor.name} #proud i've
+    #become more comfy with using curly brackets! 
   end
 
   def vendors_that_sell(items)
     @vendors.select { |vendor| vendor.inventory.include?(items)}
+  end
+
+  def total_inventory
+   total_inventory = Hash.new(0)
+    @vendors.each do |vendor| #returns vendors and their items
+      # require 'pry'; binding.pry
+      vendor.inventory.each do |item, amount| #goes over their items and amounts
+        if total_inventory.keys.include?(item) #confirms their keys hold the details of item
+          total_inventory[item][:quantity] += amount
+        else #THIS TOOK ME SO LONG I AM SO PROUD
+          total_inventory[item] = {quantity: amount, vendors: vendors_that_sell(item)}
+        end
+      end#originally had total_inventory returning on line 33, was throwing an error.  
+    end 
+    total_inventory #i hate hashes. 
+  end
+
+  def overstocked_items#sold by more than 1 vendor AND the total quantity is greater than 50.
+  #  total_inventory...... need to go through it and return count and who 
+  #it is sold by.
+  total_inventory.select do |item, quantity|
+     #item, details?
+    #quantity[:quantity...???]
   end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -13,11 +13,6 @@ RSpec.describe Market do
     @item2 = Item.new({name: 'Tomato', price: "$0.50"})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-    @vendor1.stock(@item1, 35)
-    @vendor1.stock(@item2, 7)
-    @vendor2.stock(@item4, 50)
-    @vendor2.stock(@item3, 25)
-    @vendor3.stock(@item1, 65)
   end
 
   it 'exists and has readable attributes' do 
@@ -51,8 +46,49 @@ RSpec.describe Market do
     @market.add_vendor(@vendor1)
     @market.add_vendor(@vendor2)
     @market.add_vendor(@vendor3)
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
 
     expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
     expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
+  end 
+
+  it 'can list the total inventory for vendors' do 
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+
+    expect(@market.total_inventory).to eq({
+      @item1 => {quantity: 100, vendors: [@vendor1, @vendor3]},
+      @item2 => {quantity: 7, vendors: [@vendor1]},
+      @item4 => {quantity: 50, vendors: [@vendor2]},
+      @item3 => {quantity: 35, vendors: [@vendor2, @vendor3]}
+    })
+  end #this took me a long time to sort through, am proud of it. 
+
+  it 'can list overstocked items' do 
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+
+
+    expect(@market.overstocked_items).to eq([@item1])
   end
 end
